0149a2ed51e6c472efe2a7e54d20fb575fbf9abb
felipe borges <fg662031@gmail.com>
2023-08-20 15:02:21 -0300

    excecao personalizada para objeto nao encontrado

--- src/main/java/com/felipe/bookstore/service/CategoriaService.java
+++ src/main/java/com/felipe/bookstore/service/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.felipe.bookstore.domain.Categoria;
 import com.felipe.bookstore.repositories.CategoriaRepository;
+import com.felipe.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + "Tipo: " + Categoria.class.getName()));
 	}
 
 }
